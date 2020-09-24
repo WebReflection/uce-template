dff862ab9c597d017df5ae7062264fb3a3d76ebf
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-09-24 15:47:28 +0200

    using .uce as extension

--- test/my-counter/my-counter.js
+++ test/my-counter/my-counter.js
@@ -1,6 +1,6 @@
 Promise.all([
   customElements.whenDefined('uce-template'),
-  fetch("my-counter.html").then((b) => b.text())
+  fetch('my-counter.uce').then((b) => b.text())
 ]).then(([Template, parts]) => {
   document.body.appendChild(Template.from(parts));
 });
