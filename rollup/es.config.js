b487e31bf48954be36b6a633143ff686c2b79ee3
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-09-29 16:39:43 +0200

    smaller es.js bundle

--- rollup/es.config.js
+++ rollup/es.config.js
@@ -1,9 +1,16 @@
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import {terser} from 'rollup-plugin-terser';
+import includePaths from 'rollup-plugin-includepaths';
 
 export default {
   input: './esm/index.js',
   plugins: [
+    includePaths({
+      include: {
+        '@ungap/create-content': 'node_modules/@ungap/degap/create-content.js',
+        '@ungap/custom-elements': 'node_modules/@ungap/degap/custom-elements.js'
+      },
+    }),
     nodeResolve(),
     terser()
   ],
